9c15e5ad952d552bac2c9fc76ffb456b6408cd11
unknown <atul.thakur1837@gmail.com>
2022-07-28 19:15:46 +0530

    updated factory contracts to store metadata for brands

--- contracts/factory.sol
+++ contracts/factory.sol
@@ -7,11 +7,13 @@ import "./warranty.sol";
 contract WarrantyFactory {
 
     mapping(address => WarrantyCardContract) public deployedContracts;
+    mapping(address => string) public tokenURI;
     address[] public ownersAddress;
 
-    function createNewWarrantyContract(string memory name, string memory symbol) public returns(WarrantyCardContract) {
+    function createNewWarrantyContract(string memory name, string memory symbol, string memory tokenuri) public returns(WarrantyCardContract) {
         WarrantyCardContract wc_contract = new WarrantyCardContract(name, symbol, msg.sender);
         deployedContracts[msg.sender] = wc_contract;
+        tokenURI[msg.sender] = tokenuri;
         ownersAddress.push(msg.sender);
         return wc_contract;
     }
---
+++ react-app/src/components/BrandDetails.js
@@ -0,0 +1,13 @@
+import MintContract from "./MintContract";
+import MintNFT from "./MintNFT";
+import NFTList from "./NFTList";
+
+const BrandDetails = () => {
+  return (
+    <>
+        <div>Hello, world</div>
+    </>
+  );
+};
+
+export default BrandDetails;
